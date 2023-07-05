3fa8384d91bd1de7e57872f73643c2fcb055ca0b
leonelborgesdev <leonel03t@gmail.com>
2023-07-05 10:12:08 -0400

    getproductById

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -10,11 +10,15 @@ export const createProduct = async (req, res) => {
   res.status(201).json(productSaved);
 };
 
-export const getProducts = (req, res) => {
-  res.json("get products");
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
 };
 
-export const getProductById = (req, res) => {};
+export const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.productId);
+  res.status(200).json(product);
+};
 
 export const updateProductById = (req, res) => {};
 
