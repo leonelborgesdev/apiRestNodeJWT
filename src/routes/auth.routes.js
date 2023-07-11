f0cb2952cfaede965a3cba39fef8976efd93471d
leonelborgesdev <leonel03t@gmail.com>
2023-07-11 13:48:25 -0400

    verifiNameOrEmaildDuplicate

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,13 @@ import { Router } from "express";
 const router = Router();
 
 import * as authCrtl from "../controllers/auth.controller";
+import { verifySingup } from "../middlewares";
 
-router.post("/singup", authCrtl.singup);
+router.post(
+  "/singup",
+  [verifySingup.checkDuplicateUsernameOrEmail, verifySingup.checkRolesExisted],
+  authCrtl.singup
+);
 
 router.post("/singin", authCrtl.singin);
 
