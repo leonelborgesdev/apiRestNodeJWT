33448ba33ba1526635b5e3f211e058a36f229ac9
leonelborgesdev <leonel03t@gmail.com>
2023-07-11 10:26:41 -0400

    roles auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const singup = async (req, res) => {
   const { username, email, password, roles } = req.body;
@@ -10,9 +11,16 @@ export const singup = async (req, res) => {
     email,
     password: await User.encryptPassword(password),
   });
-  console.log(newUser);
-  const savedUser = await newUser.save();
+  if (roles) {
+    const foundRoles = await Role.find({ name: { $in: roles } });
+    newUser.roles = foundRoles.map((role) => role._id);
+  } else {
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
+  }
 
+  const savedUser = await newUser.save();
+  console.log(savedUser);
   const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
     expiresIn: 86400, //24 hours
   });
